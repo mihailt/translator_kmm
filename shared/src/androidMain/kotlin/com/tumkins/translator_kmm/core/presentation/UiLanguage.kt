1c927482689917c2f204fbb15ecbf2f385953ed5
Mihails Tumkins <mihailt@gmail.com>
2023-08-18 22:36:46 +0300

    add unit tests for common

--- shared/src/androidMain/kotlin/com/tumkins/translator_kmm/core/presentation/UiLanguage.kt
+++ shared/src/androidMain/kotlin/com/tumkins/translator_kmm/core/presentation/UiLanguage.kt
@@ -5,7 +5,7 @@ import com.tumkins.translator_kmm.R
 import com.tumkins.translator_kmm.core.domain.language.Language
 import java.util.Locale
 
-actual class UiLanguage(
+actual data class UiLanguage(
     @DrawableRes val drawableRes: Int,
     actual val language: Language
 ) {
