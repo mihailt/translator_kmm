dc506a3b66c8ca07bfc485f0f7d4a1bf54df18a5
Mihails Tumkins <mihailt@gmail.com>
2023-08-18 18:12:55 +0300

    add ios history item

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,12 +4,16 @@ import shared
 struct ContentView: View {
     private let appModule = AppModule()
 
-	var body: some View {
-        TranslateScreen(
-            historyDataSource: appModule.historyDataSource,
-            translateUseCase: appModule.translateUseCase
-        )
-	}
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            TranslateScreen(
+                historyDataSource: appModule.historyDataSource,
+                translateUseCase: appModule.translateUseCase
+            )
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
